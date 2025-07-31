2eee4695f5e41c5d7470b7bba8c448fcb02252a9
nonestack <870913978@qq.com>
2025-07-31 17:23:31 +0800

    complete primes.c

--- user/primes.c
+++ user/primes.c
@@ -14,30 +14,25 @@ void prime_sieve(int *p2c)
   int p1[2];  // p -> c
   pipe(p1);
 
-  if (fork() > 0) {
+  if (fork() > 0) {  // parent
     close(p1[0]);
+    fprintf(1, "prime %d\n", start);
     int num;
     while (read(p2c[0], &num, sizeof(num)) > 0) {
       if (num % start != 0) {
         write(p1[1], &num, sizeof(num));
       }
     }
+    close(p2c[0]);
+    close(p1[1]);
+    wait(0);
+  }
+  else {  // child
+    close(p1[1]);
+    prime_sieve(p1);
+    exit(0);
   }
   
-  close(read_pipe[1]);
-  close(write_pipe[0]);
-  int start;
-  read(read_pipe[0], &start, sizeof(start));
-  fprintf(1, "prime %d\n", start);
-  int num;
-  while (read(read_pipe[0], &num, sizeof(num)) > 0) {
-    if (num % start != 0) {
-      write(write_pipe[1], &num, sizeof(num));
-    }
-  }
-  close(read_pipe[0]);
-  close(write_pipe[1]);
-  exit(0);
 }
 
 
@@ -50,22 +45,25 @@ main(int argc, char *argv[])
   int p1[2];  // p -> c
 
   pipe(p1);
-  pipe(p2);
 
   if (fork() > 0) {
     close(p1[0]);
-
-    int num = start + 1;
-
+    fprintf(1, "prime %d\n", start);
+   
     for (int i = start + 1; i <= end; ++i) {
-      if (i % start != 0) write(p[0], &i, sizeof(i));
+      if (i % start != 0) {
+        // fprintf(1, "current %d is primes\n", i);
+        write(p1[1], &i, sizeof(i));
+      }
       
     }
     close(p1[1]);
     wait(0);
   }
   else {
-    
+    close(p1[1]);
+    prime_sieve(p1);
+    exit(0);
   }
 
   exit(0);
